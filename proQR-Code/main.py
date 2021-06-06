dfb9be0ad434d4687a3fdd6c7085c93d01494237
Carlos Eduardo Maia <84158616+Maia-code@users.noreply.github.com>
2021-06-06 14:53:13 -0300

    Create main.py

---
+++ proQR-Code/main.py
@@ -0,0 +1,14 @@
+import qrcode
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction= qrcode.constants,
+    box_size=30,
+    border=2,
+)
+
+qr.add_data('Gerador de QRCode py')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save('qrcode.png')
